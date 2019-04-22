c1bdb332624c682532d1df956a6df544e8d99f4c
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-04-22 16:25:07 -0500

    test: Added test for successful git clone with auth

--- tests/unit/test_clone.py
+++ tests/unit/test_clone.py
@@ -2,7 +2,7 @@ from subprocess import PIPE
 
 from mock import patch, call, Mock
 
-from jupyterlab_git.git import Git
+from jupyterlab_git.git import Git, git_auth_input_wrapper
 
 
 @patch('subprocess.Popen')
@@ -64,3 +64,33 @@ def test_git_clone_failure_from_git(mock_subproc_popen):
         call().communicate()
     ])
     assert {'code': 128, 'message': 'fatal: Not a git repository'} == actual_response
+
+@patch('jupyterlab_git.git.git_auth_input_wrapper')
+def test_git_clone_with_auth_success(mock_git_auth_input_wrapper):
+    # Given
+    process_mock = Mock()
+    attrs = {
+        'communicate.return_value': ('output', 'error'.encode('utf-8')),
+        'returncode': 0
+    }
+    process_mock.configure_mock(**attrs)
+    mock_git_auth_input_wrapper.return_value = process_mock
+
+    # When
+    auth = {
+        'username' : 'asdf', 
+        'password' : 'qwerty'
+    }
+    actual_response = Git(root_dir='/bin').clone(current_path='test_curr_path', repo_url='ghjkhjkl', auth=auth)
+
+    # Then
+    mock_git_auth_input_wrapper.assert_has_calls([
+        call(
+            command = 'git clone ghjkhjkl -q',
+            cwd = '/bin/test_curr_path',
+            username = 'asdf',
+            password = 'qwerty'
+        ),
+        call().communicate()
+    ])
+    assert {'code': 0} == actual_response
